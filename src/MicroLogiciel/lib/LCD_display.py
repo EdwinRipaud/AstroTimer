575794b59a5c0e4f0f39570be84128ff9bd14a86
Er-berry <edwin.ripaud@wanadoo.fr>
2024-06-09 11:10:56 +0200

    Merge LCD related function to LCD_display.py

--- src/MicroLogiciel/lib/LCD_display.py
+++ src/MicroLogiciel/lib/LCD_display.py
@@ -8,8 +8,6 @@ from PIL import Image
 OPERATING_SYSTEM = os.uname()
 RUN_ON_RPi = (OPERATING_SYSTEM.sysname == 'Linux') and (OPERATING_SYSTEM.machine in ['aarch64', 'armv6l'])
 
-if RUN_ON_RPi:
-    from . import lcdconfig
 
 
 SCRIPT_NAME = __file__.split('/')[-1]
@@ -33,13 +31,105 @@ TRANSVERSE     	= 6
 #   alpha_img = Image.new(mode="RGB", size=self.LCD.size[::-1], color=(255)).split()[0]
 #   red_frame = Image.merge('RGBA', (self.LCD.screen_img.split()[0], black_img, black_img, alpha_img))
 #   self.LCD.screen_img = Image.blend(self.LCD.screen_img, red_frame, alpha=0.6)
+
+if RUN_ON_RPi:
+    import spidev
+    class RaspberryPi:
+        class_logger = logging.getLogger('displayLogger')
+        def __init__(self, spi_bus=0, spi_device=0, spi_freq=40000000, rst=27, dc=25, bl=18, bl_freq=1000):
+            self.class_logger.debug("initialise display interface with RaspberryPi",
+                                    extra={'className':f"{self.__class__.__name__}:"})
+            import RPi.GPIO
+            self.RST_PIN    = rst
+            self.DC_PIN     = dc
+            self.BL_PIN     = bl
+            self.SPEED      = spi_freq
+            self.BL_freq    = bl_freq
+            self.GPIO       = RPi.GPIO
+            
+            self.GPIO.setmode(self.GPIO.BCM)
+            self.GPIO.setwarnings(False)
+            self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
+            self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
+            self.GPIO.setup(self.BL_PIN, self.GPIO.OUT)
+            self.GPIO.output(self.BL_PIN, self.GPIO.HIGH)
+            #Initialize SPI
+            self.SPI = spidev.SpiDev(spi_bus, spi_device)
+            if self.SPI!=None :
+                self.SPI.max_speed_hz = spi_freq
+                self.SPI.mode = 0b00
+            return None
+        
+        def digital_write(self, pin, value):
+            self.class_logger.info("set GPIO pin state",
+                                   extra={'className':f"{self.__class__.__name__}:"})
+            self.GPIO.output(pin, value)
+            return None
+        
+        def digital_read(self, pin):
+            self.class_logger.info("read GPIO pin state",
+                                   extra={'className':f"{self.__class__.__name__}:"})
+            return self.GPIO.input(pin)
+    
+        def delay_ms(self, delaytime):
+            self.class_logger.info("wait delay",
+                                   extra={'className':f"{self.__class__.__name__}:"})
+            time.sleep(delaytime / 1000.0)
+            return None
+        
+        def spi_writebyte(self, data):
+            self.class_logger.info("write data to SPI bus",
+                                   extra={'className':f"{self.__class__.__name__}:"})
+            if self.SPI!=None :
+                self.SPI.writebytes(data)
+            return None
+        
+        def set_bl_DutyCycle(self, duty):
+            self.class_logger.info("set display backlight brightness",
+                                   extra={'className':f"{self.__class__.__name__}:"})
+            self._pwm.ChangeDutyCycle(duty)
+            return None
+        
+        def set_bl_Frequency(self,freq):
+            self.class_logger.info("set display response time",
+                                   extra={'className':f"{self.__class__.__name__}:"})
+            self._pwm.ChangeFrequency(freq)
+            return None
+        
+        def module_init(self):
+            self.class_logger.debug("initialise display backlight and SPI communication",
+                                    extra={'className':f"{self.__class__.__name__}:"})
+            self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
+            self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
+            self.GPIO.setup(self.BL_PIN, self.GPIO.OUT)
+            self._pwm=self.GPIO.PWM(self.BL_PIN, self.BL_freq)
+            self._pwm.start(100)
+            if self.SPI!=None :
+                self.SPI.max_speed_hz = self.SPEED
+                self.SPI.mode = 0b00
+            return None
+        
+        def module_exit(self):
+            self.class_logger.debug("SPI end and GPIO cleanup",
+                                    extra={'className':f"{self.__class__.__name__}:"})
+            if self.SPI!=None :
+                self.SPI.close()
+            
+            self.GPIO.output(self.RST_PIN, 1)
+            self.GPIO.output(self.DC_PIN, 0)
+            self._pwm.stop()
+            time.sleep(0.001)
+            self.GPIO.output(self.BL_PIN, 1)
+            return None
+
+
 class LCD_1inch47():
     class_logger = logging.getLogger('displayLogger')
     def __init__(self, spi_bus=0, spi_device=0, spi_freq=40000000, rst=27, dc=25, bl=18, bl_freq=1000):
         self.class_logger.info("initialise LCD_1inch47 display",
                                extra={'className':f"{self.__class__.__name__}:"})
         if RUN_ON_RPi:
-            self.instance = lcdconfig.RaspberryPi(spi_bus, spi_device, spi_freq, rst, dc, bl, bl_freq)
+            self.instance = RaspberryPi(spi_bus, spi_device, spi_freq, rst, dc, bl, bl_freq)
             self.Init()
         
         self.width = 172
